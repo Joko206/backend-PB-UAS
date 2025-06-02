bf83b9027206e350313c7b3d50882ef71c74b37c
Joko206 <12350110343@students.uin-suska.ac.id>
2025-06-02 23:17:07 +0700

    update CORS

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/Joko206/UAS_PWEB1/database"
 	"github.com/Joko206/UAS_PWEB1/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"strings"
 )
 
 func main() {
@@ -11,6 +13,20 @@ func main() {
 	database.GetDBConnection()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173, https://brainquiz-psi.vercel.app/",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodHead,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
+		AllowHeaders:     "",
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
